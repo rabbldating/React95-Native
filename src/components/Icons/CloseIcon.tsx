10070f689524e5367c42c90497ff02402a57adbb
Rabbl <root@rabbldating.com>
2026-03-01 10:25:05 -0600

    Bigger close icon
    Signed-off-by: Rabbl <root@rabbldating.com>

--- src/components/Icons/CloseIcon.tsx
+++ src/components/Icons/CloseIcon.tsx
@@ -55,17 +55,23 @@ const CloseIcon = ({
   return (
     <View style={style} {...rest}>
       <Image
-        source={!disabled ? require('../../assets/images/close-icon10x11.png') : require('../../assets/images/close-icon10x11disabled.png')}
-        style={styles.image}
+        source={!disabled ? require('../../assets/images/close-icon18x16.png') : require('../../assets/images/close-icon18x16disabled.png')}
+        style={styles.image18x16}
+        //source={!disabled ? require('../../assets/images/close-icon10x11.png') : require('../../assets/images/close-icon10x11disabled.png')}
+        //style={styles.image10x11}
       />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  image: {
+  image10x11: {
     width: 10,
     height: 11,
+  },
+  image18x16: {
+    width: 18,
+    height: 16,
   },
 });
 
